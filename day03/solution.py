eb9d76bd26d97b56e08eac41024f74fbe8fac1a4
Michael Aydinbas <michael.aydinbas@gmail.com>
2022-12-03 19:37:18 +0100

    refactor solution

--- day03/solution.py
+++ day03/solution.py
@@ -1,30 +1,48 @@
 #%%
 from string import ascii_lowercase, ascii_uppercase
 
-PRIORITIES = dict((l, i) for i, l in enumerate(ascii_lowercase + ascii_uppercase, start=1))
+PRIORITIES = dict(
+    (l, i) for i, l in enumerate(ascii_lowercase + ascii_uppercase, start=1)
+)
+
 
 def load_data(file: str) -> str:
     with open(file, "r", encoding="utf-8") as fhandle:
         return fhandle.read()
 
 
-def parse_data(content: str) -> list:
+def parse_data(content: str) -> list[str]:
     # return inventory
     return content.splitlines()
+
+
+def find_wrong_item(inventory: str) -> str:
+    inventory_mid = len(inventory) // 2
+    left, right = inventory[:inventory_mid], inventory[inventory_mid:]
+    wrong_item = set(left) & set(right)
+    assert len(wrong_item) == 1
+    return wrong_item.pop()
 
 
 def task1(file: str):
     content = load_data(file)
     inventory = parse_data(content)
-    compartments = [(l[:len(l)//2], l[len(l)//2:]) for l in inventory]
-    return sum(PRIORITIES.get((set(l) & set(r)).pop(), 0) for (l, r) in compartments)
+    wrong_items = map(find_wrong_item, inventory)
+    return sum(PRIORITIES.get(item, 0) for item in wrong_items)
+
+
+### PART II
+def find_batch_item(group: list[str]) -> str:
+    badge_item = set.intersection(*map(set, group))
+    assert len(badge_item) == 1
+    return badge_item.pop()
 
 
 def task2(file: str):
     content = load_data(file)
     inventory = parse_data(content)
-    badges = [(set(i1) & set(i2) & set(i3)).pop() for (i1, i2, i3) in zip(inventory[::3], inventory[1::3], inventory[2::3])]
-    return sum(PRIORITIES.get(b, 0) for b in badges)
+    groups = [iter(inventory)] * 3
+    return sum(PRIORITIES.get(find_batch_item(group), 0) for group in zip(*groups))
 
 
 #%%
